cd3e5862b43818252d0ee2cc151b66ed252aa736
Hatem-Idris <hatem.h.idris@gmail.com>
2025-09-11 11:31:46 +0300

    Crud system React

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ export default function App() {
     let copy = [...phones];
     copy.push(newObj);
     setPhones(copy);
+    showphone()
   };
   const deletephone = () => {
     let deleteindex = +prompt("Please enter phone index you want to delete");
